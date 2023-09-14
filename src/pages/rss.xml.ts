ed2c7a6f3b78f5f01e7734133002c1613de0baf7
Ángel González Vázquez <contact@angelnext.dev>
2023-09-14 10:26:59 +0200

    chore: more features

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import rss from "@astrojs/rss";
+import { type APIContext } from "astro";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
 	const posts = await getCollection("blog");
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site,
+		site: context.site ?? new URL("https://angelnext.dev/"),
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,

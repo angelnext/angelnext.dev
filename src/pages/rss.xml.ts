c757863c7a4260713f188ecec121aa8110336fcf
Ángel González Vázquez <contact@angelnext.dev>
2023-10-16 21:31:57 +0200

    style: Remove some comments and update some spacing

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,14 +4,14 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 import { getCollection } from "astro:content";
 
 export async function GET(context: APIContext) {
-  const posts = await getCollection("blog");
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site ?? new URL("https://angelnext.dev/"),
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
-  });
+	const posts = await getCollection("blog");
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site ?? new URL("https://angelnext.dev/"),
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/blog/${post.slug}/`,
+		})),
+	});
 }
